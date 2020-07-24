c8fbf306ba667d4a3d3cb23e73c4f16dfda3f4fe
tomalatv <toni.latvakoski@gmail.com>
2020-07-24 14:37:33 +0300

    Experimenting with react hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+function Count() {
+  // const evenOdd = useEvenOdd();
+  // const [count, setCount] = useState(0);
+  // let [state, dispatch] = useReducer((state, action)=>{
+  //   switch(action.type){
+  //     case 'ADD':
+  //       return {
+  //         // ...state, 
+  //         count: state.count +1,
+  //       }
+  //       case 'REDUCE':
+  //       default: return {
+  //         // ...state, 
+  //         count: state.count -1,
+  //       };
+  //   }
+  // }, { count: 0 });
+
+  // useEffect(()=>{
+  //   if(state.count % 2 === 0) {
+  //     document.title = 'even ' +state.count +' ***';
+  //   } else {
+  //     document.title = 'odd ' +state.count +' ***';
+  //   }
+  // },[state.count])  
+  const count = useEvenOdd();
+  const test = useTest();
+  return (
+    <React.Fragment>
+    <button onClick={() => test.dispatch({type: 'ADD'})}> click me ! {test.state.count}</button>
+    <button onClick={() => test.dispatch({type: 'REDUCE'})}> reduce click me ! {test.state.count}</button>
+    <button onClick={() => count.c()}> reduce click me ! {count.count}</button>
+    </React.Fragment>
+  );
+}
 
 function App() {
   return (
@@ -18,9 +55,54 @@ function App() {
         >
           Learn React
         </a>
+    <React.Fragment>
+
+    <Count />
+    </React.Fragment>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function useTest() {
+  const [state, dispatch] = useReducer((state, action)=>{
+    switch(action.type){
+      case 'ADD':
+        return {
+          // ...state, 
+          count: state.count +1,
+        }
+        case 'REDUCE':
+        default: return {
+          // ...state, 
+          count: state.count -1,
+        };
+    }
+  }, { count: 0 });
+
+  useEffect(()=>{
+    if(state.count % 2 === 0) {
+      document.title = 'even ' +state.count +' ***';
+    } else {
+      document.title = 'odd ' +state.count +' ***';
+    }
+  },[state.count])
+  return {state, dispatch};
+}
+
+function useEvenOdd(){
+  const [count, setCount] = useState(0);
+  useEffect(()=>{
+    if(count % 2 === 0) {
+      document.title = 'even ' +count +' ###';
+    } else {
+      document.title = 'odd ' +count +' ####';
+     }
+  })
+  const c = function handleCount() {
+    setCount(count +1);
+  }
+  return {c, count};
+}
